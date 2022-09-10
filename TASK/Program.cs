4f582227409aae705e224d3051c574316186b3ab
kristina <arlauskaitekristina@mail.ru>
2022-09-10 17:44:00 +0300

    Добавлена задача

---
+++ TASK/Program.cs
@@ -0,0 +1,28 @@
+﻿string[] myArray = new string[5] { "hello", "2", "pink", "computer", ":)" };
+string[] finResult = new string[myArray.Length];
+
+string[] ResultArray(string[] myArray, string[] finResult)
+{
+    int count = 0;
+    for (int i = 0; i < myArray.Length; i++)
+    {
+        if (myArray[i].Length <= 3)
+        {
+            finResult[count] = myArray[i];
+            count++;
+        }
+    }
+    return finResult;
+}
+
+void PrintArray(string[] myArray)
+{
+    for (int i = 0; i < myArray.Length; i++)
+    {
+        Console.Write(myArray[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+string[] arrayResult = ResultArray(myArray, finResult);
+PrintArray(finResult);
